52b236c67df689a04ec8c6256bd87cb51e981f0b
Nguyen Hin <tintran2k4@gmail.com>
2025-12-27 14:52:40 +0700

    Cập nhật code mới nhất

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', 
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // 1. Dòng này giúp xuất ra file HTML tĩnh (thư mục 'out')
+  output: 'export',
+  
+  // 2. Tắt tối ưu hóa ảnh mặc định (vì GitHub Pages không có server xử lý ảnh)
   images: {
     unoptimized: true,
   },
+
+  // 3. QUAN TRỌNG: Nếu bạn chưa có tên miền riêng, phải thêm tên repo vào đây
+  // Nếu repo bạn tên là 'ttinportfolio', hãy để dòng dưới. 
+  // Nếu sau này mua tên miền riêng (.com) thì xóa dòng này đi.
+  basePath: "/ttinportfolio",
 };
 
 export default nextConfig;
