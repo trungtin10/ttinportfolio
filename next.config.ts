0d5fe3dda284a96211f52a422933275da99f437c
Nguyen Hin <tintran2k4@gmail.com>
2025-12-27 15:01:32 +0700

    Them file .nojekyll va fix config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 1. Dòng này giúp xuất ra file HTML tĩnh (thư mục 'out')
   output: 'export',
-  
-  // 2. Tắt tối ưu hóa ảnh mặc định (vì GitHub Pages không có server xử lý ảnh)
   images: {
     unoptimized: true,
   },
-
-  // 3. QUAN TRỌNG: Nếu bạn chưa có tên miền riêng, phải thêm tên repo vào đây
-  // Nếu repo bạn tên là 'ttinportfolio', hãy để dòng dưới. 
-  // Nếu sau này mua tên miền riêng (.com) thì xóa dòng này đi.
-  basePath: "/ttinportfolio",
+  // Chắc chắn dòng này phải đúng tên repo của bạn
+  basePath: "/ttinportfolio", 
 };
 
 export default nextConfig;
